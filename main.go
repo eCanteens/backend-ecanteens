550a3184b829817a906ad7a1f28de9b395c0e5be
Badzlan Nur Dhabith <badzlandhabith05@gmail.com>
2024-03-25 16:19:02 +0700

    feat: add config connection db

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"github.com/eCanteens/backend-ecanteens/src/config"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		panic("Error loading env")
+	}
+}
 
 func main() {
 	router := gin.Default()
+	config.ConnectDB()
 
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "API Docs on /api"})
--- src/config/connection.go
+++ src/config/connection.go
@@ -1,1 +1,24 @@
 package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/eCanteens/backend-ecanteens/src/helpers"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	database, err := gorm.Open(postgres.Open(os.Getenv("DATABASE_URL")))
+
+	helpers.ErrorPanic(err)
+
+	fmt.Print("Database connected!")
+
+	database.AutoMigrate()
+
+	DB = database
+}
