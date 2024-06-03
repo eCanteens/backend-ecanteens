f54653147387ea11c094caa25c7905fc65bc8419
Badzlan Nur Dhabith <badzlandhabith05@gmail.com>
2024-06-03 18:51:30 +0700

    feat: add menu routes and controller

---
+++ src/modules/restaurant/menu/menu_controller.go
@@ -0,0 +1,13 @@
+package menu
+
+import "github.com/gin-gonic/gin"
+
+func handleGetAllMenu(c *gin.Context) {}
+
+func handleCreateMenu(c *gin.Context) {}
+
+func handleGetOneMenu(c *gin.Context) {}
+
+func handleUpdateMenu(c *gin.Context) {}
+
+func handleDeleteMenu(c *gin.Context) {}
